bc25eb21de6741fd144556aa9302cabdc93f9b54
Ibrahim1Hassan <hema.rashad@gmail.com>
2026-07-29 00:22:16 +0200

    decreased clock speed to match the slow logic analyzer (max divider)
    -Mode fault detected in the SR register
    -Clock and data are not generated --> mode fault disabled the spi

--- project_LEDMATRIX_MAX7219/src/drivers/spi.c
+++ project_LEDMATRIX_MAX7219/src/drivers/spi.c
@@ -6,13 +6,13 @@ void SpiInit(void){
 	// TODO: Enable SPI and GPIO ports using RCC
 	RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
 	RCC->APBENR2 |= RCC_APBENR2_SPI1EN;
-	// TODO: Configure GPIOs for MOSI PA7, MISO(not needed), SCK PA5, CS PA4
-	// PA5->AF0 0x0 and PA7->AF0 0x0 as AF
-	GPIOA->MODER |= GPIO_MODER_MODE5_1;
-	GPIOA->MODER &= ~GPIO_MODER_MODE5_0;
+	// TODO: Configure GPIOs for MOSI PA7, MISO(not needed), SCK PA1, CS PA4
+	// PA1->AF0 0x0 and PA7->AF0 0x0 as AF
+	GPIOA->MODER |= GPIO_MODER_MODE1_1;
+	GPIOA->MODER &= ~GPIO_MODER_MODE1_0;
 	GPIOA->MODER |= GPIO_MODER_MODE7_1;
 	GPIOA->MODER &= ~GPIO_MODER_MODE7_0;
-	GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL5_0 | GPIO_AFRL_AFSEL5_1 | GPIO_AFRL_AFSEL5_2 | GPIO_AFRL_AFSEL5_3);
+	GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL1_0 | GPIO_AFRL_AFSEL1_1 | GPIO_AFRL_AFSEL1_2 | GPIO_AFRL_AFSEL1_3);
 	GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL7_0 | GPIO_AFRL_AFSEL7_1 | GPIO_AFRL_AFSEL7_2 | GPIO_AFRL_AFSEL7_3);
 	// PA4 as output, push-pull
 	// To set PA4 high later GPIOA->BSRR = GPIO_BSRR_BS4; //(no need for bitwise OR)
@@ -29,9 +29,10 @@ void SpiInit(void){
 	SPI1->CR1 &= ~SPI_CR1_CPOL;
 	// Clock phase rising edge (first clock transition)
 	SPI1->CR1 &= ~SPI_CR1_CPHA;
-	// BaudRate divider of 1/2 0x0 of the source APB clock (12 MHz)
+	// BaudRate divider of 1/16 0b011 of the source APB clock (12 MHz)
 	// IC max operating freq is 10MHz
-	SPI1->CR1 &= ~(SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_BR_2);
+//	SPI1->CR1 &= ~(SPI_CR1_BR_2);
+	SPI1->CR1 |= (SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0);
 	// SPI Master Mode
 	SPI1->CR1 |= SPI_CR1_MSTR;
 	// Software slave mangement enabled (internal slave select NSS disabled)
